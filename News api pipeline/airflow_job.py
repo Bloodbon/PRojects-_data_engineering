1975193438ee3245e13e87737d53329d97bc6451
Bloodbon <biswalsatish55@gmail.com>
2024-11-27 07:07:50 +0530

    Update airflow_job.py

--- News api pipeline/airflow_job.py
+++ News api pipeline/airflow_job.py
@@ -5,7 +5,7 @@ from fetch_news import fetch_news_data
 from airflow.contrib.operators.snowflake_operator import SnowflakeOperator
 
 default_args = {
-    'owner': 'growdataskills',
+    'owner': 'datawale',
     'depends_on_past': False,
     'email_on_failure': False,
     'email_on_retry': False,
